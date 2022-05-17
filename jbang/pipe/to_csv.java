65ee61ee9db1491f3fc6bee0332c1f4e580c9a06
Claus Ibsen <claus.ibsen@gmail.com>
2022-05-17 10:54:01 +0200

    jbang pipe example

---
+++ jbang/pipe/to_csv.java
@@ -0,0 +1,21 @@
+///usr/bin/env jbang --quiet camel@apache/camel pipe "$0" "$@" ; exit $?
+// camel-k: language=java
+
+import org.apache.camel.builder.AggregationStrategies;
+import org.apache.camel.builder.RouteBuilder;
+
+public class to_csv extends RouteBuilder {
+
+    @Override
+    public void configure() throws Exception {
+        // read from system:in
+        from("stream:in")
+            // aggregate all lines together using comma as separator
+            .aggregate(constant("true")).completionTimeout(100)
+              .aggregationStrategy(AggregationStrategies.string(","))
+              // append new line to mark end of line
+              .setBody().simple("${body}\n")
+              // write to system:out
+              .to("stream:out");
+    }
+}
