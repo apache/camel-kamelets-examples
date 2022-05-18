0c59ac186a300a654737bf4e1abc7f7c3091cbf8
Claus Ibsen <claus.ibsen@gmail.com>
2022-05-18 13:24:49 +0200

    CAMEL-18118: camel-jbang - Linux terminal scripting using pipes

--- jbang/pipe/to_csv.java
+++ jbang/pipe/to_csv.java
@@ -6,13 +6,16 @@ import org.apache.camel.builder.RouteBuilder;
 
 public class to_csv extends RouteBuilder {
 
+    @org.apache.camel.PropertyInject(value = "separator", defaultValue = ",")
+    private String separator;
+
     @Override
     public void configure() throws Exception {
         // read from system:in
         from("stream:in")
             // aggregate all lines together using comma as separator
             .aggregate(constant("true")).completionTimeout(100)
-              .aggregationStrategy(AggregationStrategies.string(","))
+              .aggregationStrategy(AggregationStrategies.string(separator))
               // append new line to mark end of line
               .setBody().simple("${body}\n")
               // write to system:out
