a0bdf8bd3e8383c857bb49f46942e21b58cf6d48
Claus Ibsen <claus.ibsen@gmail.com>
2022-07-01 16:10:53 +0200

    A chaos monkey example

---
+++ jbang/chaos-monkey/ChaosMonkey.java
@@ -0,0 +1,30 @@
+@org.apache.camel.BindToRegistry("chaosMonkeyCheck")
+public class ChaosMonkey extends org.apache.camel.impl.health.AbstractHealthCheck {
+
+    private int num;
+
+    public ChaosMonkey() {
+        super("acme", "chaos");
+        System.out.println("Installing Chaos Monkey");
+    }
+
+    public String status(String body) {
+        num = new java.util.Random().nextInt(10);
+         if (num < 4) {
+            return "Chaos Moneky is here";
+        } else {
+            return body;    
+        }
+    }
+
+    @Override
+    public void doCall(org.apache.camel.health.HealthCheckResultBuilder builder, java.util.Map<String, Object> options) {
+        if (num < 4) {
+            builder.detail("number-number", num).down();
+        } else {
+            builder.detail("chaos-number", num).up();    
+        }
+    }
+
+
+}
