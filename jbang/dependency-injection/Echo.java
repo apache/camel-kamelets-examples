dea83cfc18cee0e43ef8c55b4f35323c31f8e1c8
Claus Ibsen <claus.ibsen@gmail.com>
2023-05-31 19:18:57 +0200

    CAMEL-19404: Add @Value in example

--- jbang/dependency-injection/Echo.java
+++ jbang/dependency-injection/Echo.java
@@ -1,6 +1,7 @@
 // camel-k: language=java
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
@@ -12,13 +13,16 @@ public class Echo {
   @Autowired
   private org.apache.camel.CamelContext context;
 
+  @Value("greeting")
+  private String prefix;
+
   @Bean
   public CamelLogger myLogger() {
     return new CamelLogger("myLogger");
   }
 
   public String echo(String echo) {
-    return echo + echo + "!! from " + context.getName();
+    return prefix + " " + echo + echo + "!! from " + context.getName();
   }
 
 
