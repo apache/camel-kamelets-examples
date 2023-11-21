216d2907e0142a527567b64d92880a1eed5ab871
Claus Ibsen <claus.ibsen@gmail.com>
2023-11-21 10:49:01 +0100

    CAMEL-20134: Added example of inlined java code for creating bean using a builder class.

---
+++ jbang/bean-inlined-code/CustomerBuilder.java
@@ -0,0 +1,34 @@
+package com.mycompany;
+
+public final class CustomerBuilder {
+
+	private String name;
+	private String street;
+	private int zip;
+	private boolean gold;
+
+	public CustomerBuilder name(String name) {
+		this.name = name;
+		return this;
+	}
+
+	public CustomerBuilder street(String street) {
+		this.street = street;
+		return this;
+	}
+
+	public CustomerBuilder zip(int zip) {
+		this.zip = zip;
+		return this;
+	}
+
+	public CustomerBuilder gold(boolean gold) {
+		this.gold = gold;
+		return this;
+	}	
+
+	public Customer build() {
+        return new Customer(name, street, zip, gold);
+	}	
+
+}
