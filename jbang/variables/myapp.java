1579ece5969e7254fea15c833042b4d17f85466a
Claus Ibsen <claus.ibsen@gmail.com>
2023-12-30 14:23:00 +0100

    CAMEL-19749: Add variables as concept to Camel

---
+++ jbang/variables/myapp.java
@@ -0,0 +1,18 @@
+// camel-k: language=java
+
+import org.apache.camel.builder.RouteBuilder;
+
+public class myapp extends RouteBuilder {
+
+    @Override
+    public void configure() throws Exception {
+        from("timer:java")
+            .setVariable("global:app", simple("${camelId}"))
+            .setVariable("global:random", simple("${random(1,10)}"));
+
+        from("timer:random?period={{time:1000}}")
+            .setBody(simple("Random number: ${variable.global:random}"))
+            .log("${body}");
+
+    }
+}
